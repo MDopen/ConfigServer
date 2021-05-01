0549712c8cb8179f022ccef597cba3648b6fc51e
Dyeimys Franco <dyeimys@hotmail.com>
2021-05-01 12:37:10 -0300

    initial commit

---
+++ src/test/java/br/com/dfranco/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dfranco.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
